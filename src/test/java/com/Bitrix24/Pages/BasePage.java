5f50678ccd5b8e05fafc6f1aaf858a091766a009
Irina Kiseleva <irina.kiseleva@icloud.com>
2020-07-23 22:00:45 -0400

    add commit

--- src/test/java/com/Bitrix24/Pages/BasePage.java
+++ src/test/java/com/Bitrix24/Pages/BasePage.java
@@ -3,7 +3,7 @@ package com.Bitrix24.Pages;
 import com.Bitrix24.Utilities.Driver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+//this is commit
 public class BasePage {
     BasePage(){
         PageFactory.initElements(Driver.getDriver(), this);
