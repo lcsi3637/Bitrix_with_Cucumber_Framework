bd6b292679959bb8d1e53c7c6ab9799739be76b2
arzuozen <arzuozen2601@gmail.com>
2020-07-23 22:04:52 -0400

    HoverOver

--- src/test/java/com/Bitrix24/Utilities/BrowserUtilities.java
+++ src/test/java/com/Bitrix24/Utilities/BrowserUtilities.java
@@ -1,6 +1,7 @@
 package com.Bitrix24.Utilities;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -12,6 +13,11 @@ public class BrowserUtilities {
         wait.until(ExpectedConditions.visibilityOf(element));
 
 
+
+    }
+    public static void hoverOverElement(WebElement element){
+        Actions action=new Actions(Driver.getDriver());
+        action.moveToElement(element).perform();
 
     }
 }
